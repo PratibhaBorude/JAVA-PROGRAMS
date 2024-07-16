4dfd9713bbcb67113374366c45049734f806964c
Pratibha Borude <140377360+PratibhaBorude@users.noreply.github.com>
2024-07-16 23:46:35 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class practice {
+public  static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+   // char A='D';
+    // take input from user
+    char A=sc.next().charAt(0);
+    char B=sc.next().charAt(0);
+    if(A>B){
+        System.out.println(A);
+    }else{
+        System.out.println(B);
+    }
+//    byte c=150;
+//    int b=sc.nextInt();
+//    a=0;
+//    String a="Ashish is a mad";
+//              012345678910111213
+}
+}
+//index
+//Datatypes int char string
+
