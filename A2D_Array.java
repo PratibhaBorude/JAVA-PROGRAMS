8c248bb96932bf2adeef0ddf441f0bc01831fa54
Pratibha Borude <140377360+PratibhaBorude@users.noreply.github.com>
2024-07-24 12:23:31 +0530

    Add files via upload

---
+++ A2D_Array.java
@@ -0,0 +1,19 @@
+public class A2D_Array {
+    public static void main(String[]args){
+        int arr1[][]={{1,2,3},
+                {4,5,6}};
+        for (int i=0;i<arr1.length;i++){
+            for (int j=0;j<arr1[i].length;j++){
+                System.out.print(arr1[i][j]+" ");
+            }
+            System.out.println( );
+
+        }
+//        for (int []row:arr1){
+//            for (int element:row){
+//                System.out.print(element+" ");
+//            }
+//System.out.println( );
+//        }
+    }
+}
